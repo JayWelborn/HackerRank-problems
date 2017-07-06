21465c83211f9386d0e4f2d9deb621595344817e
Jay Welborn <jesse.welborn@gmail.com>
2017-07-06 12:20:32 +0200

    baby's napping

---
+++ cracking interview/rotate_list.py
@@ -0,0 +1,11 @@
+def array_left_rotation(a, n, k):
+    for i in range(k):
+        a.append(a.pop(0))
+        
+    return a
+    
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
