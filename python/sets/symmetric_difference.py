4a971c23cff1a9e5475313342697f553e2dfaf82
Jay Welborn <jesse.welborn@gmail.com>
2017-07-16 21:52:58 +0200

    waiting for vagrant vm to provision

---
+++ python/sets/symmetric_difference.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+# make first set
+M = set(list(map(int, input().split())))
+
+b = int(input())
+
+# make second set
+N = set(list(map(int, input().split())))
+
+# get difference
+diff_1 = list(M.difference(N))
+diff_2 = list(N.difference(M))
+difference = diff_1 + diff_2
+
+# print difference
+[print(x) for x in sorted(difference)]
