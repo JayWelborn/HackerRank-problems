7419e6e63c372d413daefa60b75c051423ca6eae
jaywelborn <jesse.welborn@gmail.com>
2017-10-09 15:19:30 +0200

    solved Alphabet Rangoli

--- python/strings/alphabet_rangoli.py
+++ python/strings/alphabet_rangoli.py
@@ -7,8 +7,21 @@ def print_rangoli(size):
     examples at https://www.hackerrank.com/challenges/alphabet-rangoli
     """
     from string import ascii_lowercase
-    substring = ascii_lowercase[:size]
-    print(substring)
+    letters = ascii_lowercase[:size][::-1]
+
+    for i in range(1, size + 1):
+        first_half = letters[:i]
+        second_half = first_half[::-1][1:]
+        rangoli_letters = '-'.join(first_half + second_half)
+        width = size * 4 - 3
+        print(rangoli_letters).center(width, '-')
+
+    for i in range(size - 1, 0, -1):
+        first_half = letters[:i]
+        second_half = first_half[::-1][1:]
+        rangoli_letters = '-'.join(first_half + second_half)
+        width = size * 4 - 3
+        print(rangoli_letters).center(width, '-')
 
 
 if __name__ == '__main__':
