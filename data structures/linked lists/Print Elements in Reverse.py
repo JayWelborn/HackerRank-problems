3297171fdaa969055086b97e788d772017e7b205
jaywelborn <jesse.welborn@gmail.com>
2017-10-09 11:43:49 +0200

    new machine

---
+++ data structures/linked lists/Print Elements in Reverse.py
@@ -0,0 +1,24 @@
+"""
+ Print elements of a linked list in reverse order as standard output
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ 
+"""
+
+def ReversePrint(head):
+    if head == None or head.next == None:
+        pass
+    else:
+        ReversePrint(head.next)
+    
+    if head:
+        print(head.data)
+
+  
