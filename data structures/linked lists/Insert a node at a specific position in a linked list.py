66a2d9dfc8ad18a8c26cd4fbe99e80c874f26e03
jaywelborn <jesse.welborn@gmail.com>
2017-10-31 20:27:01 +0100

    solved another problem

---
+++ data structures/linked lists/Insert a node at a specific position in a linked list.py
@@ -0,0 +1,29 @@
+"""
+ Insert Node at a specific position in a linked list
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method. 
+"""
+#This is a "method-only" submission.
+#You only need to complete this method.
+def InsertNth(head, data, position):
+    if not head:
+        return Node(data=data)
+    elif position == 0:
+        return Node(data=data, next_node=head)
+    else:
+        current_node = head
+        for i in range(position - 1):
+            current_node = current_node.next
+        if current_node.next:
+            current_node.next = Node(data=data, next_node=current_node.next)
+        else:
+            current_node.next = Node(data=data)
+        return head
